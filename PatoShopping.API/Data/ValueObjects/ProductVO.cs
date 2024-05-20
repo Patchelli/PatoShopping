e1e9837a7d3f1343cb5d128f18cfbfc564589ca9
Patrick Patchelli <patrick.patchelli@hotmail.com>
2024-05-20 10:00:49 -0300

    feat: create repository and VO

---
+++ PatoShopping.API/Data/ValueObjects/ProductVO.cs
@@ -0,0 +1,15 @@
+﻿using PatoShopping.API.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace PatoShopping.API.Data.ValueObjects
+{
+    public class ProductVO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public EnumCategory Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ PatoShopping.API/Repository/Interface/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using PatoShopping.API.Data.ValueObjects;
+
+namespace PatoShopping.API.Repository.Interface
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> GetAll();
+        Task<IEnumerable<ProductVO>> FindById(long id);
+        Task<IEnumerable<ProductVO>> Create(ProductVO product);
+        Task<IEnumerable<ProductVO>> Update(ProductVO product);
+        Task<IEnumerable<ProductVO>> DeleteById(long id);
+    }
+}
